67321b9ff597d2e129b422c55f92debba21dca98
NTQhoaivu <vu.que@NTQ-solution.com.vn>
2022-11-15 10:53:37 +0700

    yeu cau so 7: nhap tuoi va hien ra man hinh

---
+++ Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        //Console.BackgroundColor = ConsoleColor.Cyan;
+        //Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Tên tôi là :" + Console.ReadLine());
+    }
+}
---
+++ Solution1/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        string old;
+
+        Boolean a;
+        int olds;
+        int check = 1;
+        do
+        {
+            Console.WriteLine("nhập tuổi của bạn:");
+            old = Console.ReadLine();
+            a = int.TryParse(old, out olds);
+
+            if (a == true)
+            {
+                Console.WriteLine("Tôi năm nay  :" + olds + " tuổi.");
+                check = 1;
+            }
+            else
+            {
+                Console.WriteLine("Hãy nhập lại đúng định dạng.");
+                check = 0;
+            }
+        }while (check == 0);
+    }
+}
