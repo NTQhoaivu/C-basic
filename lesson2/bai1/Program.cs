be93473dbf28366a06e55c5d628c3101935ceb82
NTQhoaivu <vu.que@NTQ-solution.com.vn>
2022-11-16 23:15:03 +0700

    sua loi commit ban 1 va vai 3

---
+++ lesson2/bai1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] i = { 2, 3, 1, 5, 4, 6, 8, 20, 14 };
+            int min = 100;
+            for (int j = 0; j < i.Length; j++) {
+                if (i[j] < min) {
+                    min = i[j];
+                }
+            }
+            Console.WriteLine("min : {0}",min);
+            Console.ReadLine();
+        }
+    }
+}
