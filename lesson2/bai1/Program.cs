0fa06c7b9d424ada2c5ea893c07dffa04b89c468
NTQhoaivu <vu.que@NTQ-solution.com.vn>
2022-11-18 16:03:37 +0700

    fix bai 1 lession 2

--- lesson2/bai1/Program.cs
+++ lesson2/bai1/Program.cs
@@ -7,7 +7,7 @@ namespace bai1
         static void Main(string[] args)
         {
             int[] i = { 2, 3, 1, 5, 4, 6, 8, 20, 14 };
-             void FindMin(int[] a, ref int minx)
+            void FindMin(int[] a, ref int minx)
             {
                 for (int j = 0; j < a.Length; j++)
                 {
@@ -17,9 +17,17 @@ namespace bai1
                     }
                 }
             }
-            int min = 100;
-            FindMin(i,ref min);
-            Console.WriteLine("min : {0}", min);
+            if (i[0] != null)
+            {
+                int min = i[0];
+
+                FindMin(i, ref min);
+                Console.WriteLine("min : {0}", min);
+            }
+            else
+            {
+                Console.WriteLine("wrong type");
+            }
             Console.ReadLine();
         }
     }
