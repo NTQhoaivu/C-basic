c744d9f9db38efd157e62f20d3da65c695c9a9d0
NTQhoaivu <vu.que@NTQ-solution.com.vn>
2022-11-18 15:59:52 +0700

    add deligate test

---
+++ SelfLearning/TestDeligate/Program.cs
@@ -0,0 +1,11 @@
+﻿public delegate void stringDeligate(string text);
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        stringDeligate stringa = ToUpcase;
+        stringa("test");
+    }
+    static void ToUpcase(string text) => Console.WriteLine(text.ToUpper());
+    static void ToLowcase(string text) => Console.WriteLine(text.ToLower());
+}
