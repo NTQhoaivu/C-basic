7ef123a0b566eef5d9ed577ba82a802a19de5c14
NTQhoaivu <vu.que@NTQ-solution.com.vn>
2022-11-20 23:57:07 +0700

    add number class (oop)

---
+++ SelfLearning/TestDeligate/number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDeligate
+{
+    internal class number
+    {
+        public double RealInt { get; set; }
+        public double ImaInt { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is number number &&
+                   RealInt == number.RealInt &&
+                   ImaInt == number.ImaInt;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RealInt, ImaInt);
+        }
+    }
+}
