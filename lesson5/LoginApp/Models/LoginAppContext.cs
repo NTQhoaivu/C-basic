b62d4719483411c2927e3b9530bc0ad1b8a6d134
NTQhoaivu <vu.que@NTQ-solution.com.vn>
2022-11-30 00:12:46 +0700

    update gender

--- lesson5/LoginApp/Models/LoginAppContext.cs
+++ lesson5/LoginApp/Models/LoginAppContext.cs
@@ -19,23 +19,20 @@ public partial class LoginAppContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                 => optionsBuilder.UseSqlServer("Server=DESKTOP-CT2E4DM\\SQL2022;Database=LoginApp; Trust Server Certificate=true;User Id=sa;Password=NTQ@solution01");
-
-
-
-
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-BL4VUOD;Database=LoginApp; Trust Server Certificate=true;User Id=sa;Password=NTQ@solution01");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__User__3214EC07042EB0F0");
+            entity.HasKey(e => e.Id).HasName("PK__Table__3214EC07960882A3");
 
             entity.ToTable("User");
 
             entity.Property(e => e.Address).HasMaxLength(250);
             entity.Property(e => e.BirthDay).HasColumnType("datetime");
             entity.Property(e => e.Email).HasMaxLength(250);
+            entity.Property(e => e.Gender).HasColumnName("Gender ");
             entity.Property(e => e.Password).HasMaxLength(250);
             entity.Property(e => e.UserName).HasMaxLength(150);
         });
