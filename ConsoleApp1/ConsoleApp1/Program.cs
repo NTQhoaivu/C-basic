8cb780c66f28e1f1cab1a5fe3c90018acc78243a
NTQhoaivu <vu.que@NTQ-solution.com.vn>
2022-12-02 22:48:07 +0700

    update sample middleware

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace RectangleApplication
+{
+    class Rectangle
+    {
+        protected double length;
+        protected double width;
+
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+
+        public async Task<int> DisplayAsync()
+        {
+            await Task.Delay(3000);
+            return 1;
+        }
+    }
+    class Tabletop : Rectangle
+    {
+        public Tabletop(double l, double w) : base(l, w)
+        { }
+
+        public double GetCost()
+        {
+            return GetArea() * 70;
+        }
+
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine("Cost: {0}", GetCost());
+        }
+    }
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle tabletop = new Rectangle(4, 3);
+            Task<int> a = tabletop.DisplayAsync();
+            tabletop.Display();
+            Console.ReadLine();
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract void Speak();
+
+    }
+
+}
