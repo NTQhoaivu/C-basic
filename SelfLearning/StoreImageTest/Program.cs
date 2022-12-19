b63a984f71be3309c20559e4c346d91c5615a1c8
NTQhoaivu <vu.que@NTQ-solution.com.vn>
2022-12-19 11:40:24 +0700

    update check url image

---
+++ SelfLearning/StoreImageTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace StoreImageTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var enviroment = System.Environment.CurrentDirectory;
+
+
+            Console.WriteLine("Hello, World!");
+            string projectDirectory = Directory.GetParent(enviroment).Parent.FullName;
+            var path = projectDirectory + "/Images/" ;
+            var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+
+            List<string> imageFiles = new List<string>();
+            foreach (string filename in files)
+            {
+                if (Regex.IsMatch(filename, @"\.jpg$|\.png$|\.gif$"))
+                {
+                    imageFiles.Add(filename);
+                    Console.WriteLine(filename);
+                }
+
+
+            }
+        }
+    }
+}
--- WPFLoginUseDBApp/LoginUseDbApp/ViewModels/ListRolesViewModel.cs
+++ WPFLoginUseDBApp/LoginUseDbApp/ViewModels/ListRolesViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace LoginUseDbApp.ViewModels
    
     public class ListRolesViewModel : ViewModelBase
     {
+        private var resourceManager = ResourceManager;
+
 
     }
 }
