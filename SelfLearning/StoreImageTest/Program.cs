9c066b8a622c081869ed85e0ac03348b9e0ddd9e
NTQhoaivu <vu.que@NTQ-solution.com.vn>
2022-12-19 13:32:13 +0700

    done

--- SelfLearning/StoreImageTest/Program.cs
+++ SelfLearning/StoreImageTest/Program.cs
@@ -14,21 +14,19 @@ namespace StoreImageTest
 
             Console.WriteLine("Hello, World!");
             string projectDirectory = Directory.GetParent(enviroment).Parent.FullName;
-            var path = projectDirectory + "/Images/" ;
+            var path = projectDirectory + "/Images/";
             var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
-
-
             List<string> imageFiles = new List<string>();
             foreach (string filename in files)
             {
                 if (Regex.IsMatch(filename, @"\.jpg$|\.png$|\.gif$"))
                 {
                     imageFiles.Add(filename);
-                    Console.WriteLine(filename);
+                    string s = Path.GetFileName(filename).Substring(0, Path.GetFileName(filename).Length - 3);
+                    Console.WriteLine(s);
                 }
-
-
             }
+
         }
     }
 }
